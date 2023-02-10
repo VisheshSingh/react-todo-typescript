31bb2af337aad531447ab6574b22f5c8104fcebc
Vishesh Singh <VisheshSingh16@gmail.com>
2023-02-09 20:09:48 -0500

    Use refs with form events

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import NewTodo from './NewTodo';
 import TodoList from './TodoList';
 
 const App: React.FC = () => {
-  const todos = [{id: 1, title: 'Buy milk'}, {id: 2, title: 'Get some gas'}]
+  const todos = [
+    { id: 1, title: 'Buy milk' },
+    { id: 2, title: 'Get some gas' },
+  ];
+
+  const todoHandler = (todo: string) => {
+    console.log(todo);
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
+      <NewTodo addTodo={todoHandler} />
       <TodoList items={todos} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/NewTodo.tsx
@@ -0,0 +1,24 @@
+import React, {useRef} from 'react'
+
+interface NewTodoProps {
+    addTodo: (t: string) => void
+}
+
+const NewTodo: React.FC<NewTodoProps> = ({addTodo}) => {
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault()
+        const newTodo = textInputRef.current!.value;
+        addTodo(newTodo)
+         textInputRef.current!.value = '';
+    }
+  return (
+    <form onSubmit={submitHandler}>
+        <input type="text" id="new-todo" placeholder='Add a new Todo' ref={textInputRef} />
+        <button>Submit</button>
+    </form>
+  )
+}
+
+export default NewTodo
