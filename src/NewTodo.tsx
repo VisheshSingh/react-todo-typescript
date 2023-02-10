792c5c7588c9bc602b19fcb7cb35b059487ec10b
Vishesh Singh <VisheshSingh16@gmail.com>
2023-02-09 20:10:58 -0500

    format with prettier

--- src/NewTodo.tsx
+++ src/NewTodo.tsx
@@ -1,24 +1,30 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react';
 
 interface NewTodoProps {
-    addTodo: (t: string) => void
+  addTodo: (t: string) => void;
 }
 
-const NewTodo: React.FC<NewTodoProps> = ({addTodo}) => {
-    const textInputRef = useRef<HTMLInputElement>(null);
+const NewTodo: React.FC<NewTodoProps> = ({ addTodo }) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
 
-    const submitHandler = (e: React.FormEvent) => {
-        e.preventDefault()
-        const newTodo = textInputRef.current!.value;
-        addTodo(newTodo)
-         textInputRef.current!.value = '';
-    }
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newTodo = textInputRef.current!.value;
+    addTodo(newTodo);
+    textInputRef.current!.value = '';
+  };
+
   return (
     <form onSubmit={submitHandler}>
-        <input type="text" id="new-todo" placeholder='Add a new Todo' ref={textInputRef} />
-        <button>Submit</button>
+      <input
+        type='text'
+        id='new-todo'
+        placeholder='Add a new Todo'
+        ref={textInputRef}
+      />
+      <button>Submit</button>
     </form>
-  )
-}
+  );
+};
 
-export default NewTodo
+export default NewTodo;
